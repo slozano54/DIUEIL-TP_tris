1b67c78b9604d8a21afc4c98f7d3828eff93a193
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-02-22 21:43:11 +0100

    :construction: fonction isSorted()

--- moduleTris/tri_bulles.py
+++ moduleTris/tri_bulles.py
@@ -5,6 +5,30 @@
 """
 pass
 
+import copy
+
+def isSorted(tab:list)->bool:
+    """Fonction booléenne qui retourne True si le tableau passé en paramètre est trié"""
+    #Le booléen de sorite
+    isTrue = True    
+    #la longueur du tableau
+    n=len(tab)
+    
+    #Preconditions    
+    assert isinstance(tab,list),"le paramètre n'est pas une liste"
+
+    #initialisation du compteur de boucle
+    i=0
+    while i<n-1:
+        #Invariant  : le ième élément est inférieur au (i+1)ème
+        if tab[i]>tab[i+1]:
+            isTrue = False
+        i+=1
+    
+    #Postconditions
+    assert isTrue==True,"Le tableau n'est pas trié"
+
+    return isTrue
 
 def tri_bulles(tab:list)->list:
     """Fonction de tri à bulles
@@ -16,9 +40,18 @@ def tri_bulles(tab:list)->list:
 
         * le tableau trié
     
-    `Notes`
+    `Préconditions`
     
-        * ...
+        * Le tableau n'est pas vide 
+
+    `Invariant`
+    
+        * 
+
+    `Postconditions`
+    
+        * 
+    
 
     `Exemples`
     
@@ -28,7 +61,9 @@ def tri_bulles(tab:list)->list:
     pass
 
     # On récupère la taille du tableau dans une variable
-    n = len(tab)    
+    n = len(tab)
+    # on copie le tableau d'origine
+    tab_copy = copy.deepcopy(tab)     
 
     #preconditions
     assert isinstance(tab,list),"Le parametre n'est pas une liste"
@@ -38,7 +73,11 @@ def tri_bulles(tab:list)->list:
     if n>=2 :
         # Traverser tous les éléments du tableau
         for i in range(n):
-            # INVARIANT : le tableau contient toujours n éléments
+            # INVARIANT : 
+            # le tableau contient toujours les mêmes n éléments 
+            # Les éléments n-i à n sont triés
+            print("i=",i," : ",tab)
+            assert isSorted(tab[n-i:n]),"Le tableau n'est pas trié de n-i à n"
             assert len(tab)==n,"Le nombre d'éléments du tableau a changé"            
             for j in range(0, n-i-1):
                 # Si l'élément trouvé est plus grand que le suivant on échange les deux
@@ -47,7 +86,7 @@ def tri_bulles(tab:list)->list:
 
     #postconditions
     assert len(tab)==n,"Le nombre d'éléments du tableau a changé"            
-    # il faudrait vérifier que les éléments sont restés ceux de départ peut être avec une fonction à part ?
+    # il faudrait vérifier que les éléments sont restés ceux de départ peut être avec une fonction à part ?    
     # il faudrait vérifier que le tableau est trié, peut être avec une fonction à part ?
 
     return tab
@@ -56,6 +95,9 @@ if __name__=="__main__" :
     tab_a_trier=[5,4,3,2,1]
     print(tab_a_trier)
     print(tri_bulles(tab_a_trier))
+    print(isSorted([1,2,3,4,5,6,7,8]))
+    
+
     
     
 
