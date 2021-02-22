890412d959015b275bdc3f5cbb821b70cf8340a2
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-02-22 22:40:25 +0100

    :art: ajout utils.py outils communs pour les tris

--- moduleTris/tri_bulles.py
+++ moduleTris/tri_bulles.py
@@ -5,53 +5,12 @@
 """
 pass
 
-import copy
+# On fait les imports nécessaires selon le contexte
+if __name__ == "__main__":    
+    import utils as ui    
+else:
+    import moduleTris.utils as ui   
 
-def isSorted(tab:list)->bool:
-    """Fonction booléenne qui retourne True si le tableau passé en paramètre est trié"""
-    #Le booléen de sorite
-    isTrue = True    
-    #La longueur du tableau
-    n=len(tab)
-    
-    #Preconditions    
-    assert isinstance(tab,list),"le paramètre n'est pas une liste"
-
-    #initialisation du compteur de boucle
-    i=0
-    while i<n-1:
-        #Invariant  : le ième élément est inférieur au (i+1)ème
-        if tab[i]>tab[i+1]:
-            isTrue = False
-        i+=1
-    
-    #Postconditions
-    assert isTrue==True,"Le tableau n'est pas trié"
-
-    return isTrue
-
-def permuteTab_i_j(tab:list,i:int,j:int)->list:
-    """Fonction qui permute les éléments i et j d'un tableau passé en paramètre"""
-    #La longueur du tableau
-    n=len(tab)
-
-    #Preconditions
-    assert n>0,"Le tableau est vide"
-    assert i in range(n),"i n'est pas un indice du tableau"
-    assert j in range(n),"j n'est pas un indice du tableau"
-
-    #variable tampon
-    temp = tab[i]
-    tab[i] = tab[j]
-    tab[j]= temp
-
-    #Postconditions
-    assert len(tab)==n,"Le nombre d'éléments du tableau a changé"
-    assert temp in tab,"tab[i] a disparu"
-    assert tab[i] in tab,"tab[j] a disparu"
-
-    return tab
-  
     
 def tri_bulles(tab:list)->list:
     """Fonction de tri à bulles
@@ -97,17 +56,17 @@ def tri_bulles(tab:list)->list:
             # INVARIANT :             
             # Les éléments n-i à n sont triés
             #print("i=",i," : ",tab)
-            assert isSorted(tab[n-i:n]),"Le tableau n'est pas trié de n-i à n"
+            assert ui.isSorted(tab[n-i:n]),"Le tableau n'est pas trié de n-i à n"
             assert len(tab)==n,"Le nombre d'éléments du tableau a changé"            
             for j in range(0, n-i-1):
                 # Si l'élément trouvé est plus grand que le suivant on échange les deux
                 if tab[j] > tab[j+1] :
                     #tab[j], tab[j+1] = tab[j+1], tab[j]
-                    permuteTab_i_j(tab,j,j+1)
+                    ui.permuteTab_i_j(tab,j,j+1)
 
     #postconditions
     assert len(tab)==n,"Le nombre d'éléments du tableau a changé"                
-    assert isSorted(tab),"Le tableau n'est pas trié"
+    assert ui.isSorted(tab),"Le tableau n'est pas trié"
     # il faudrait vérifier que les éléments sont restés ceux de départ peut être avec une fonction à part ?        
 
     return tab
