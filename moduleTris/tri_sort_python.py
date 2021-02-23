952020f11c374b886ed8c92111cfad4288651c16
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-02-23 18:15:58 +0100

    :construction: mesureTempsTriV3 dans le package + mise dans un dossier des fichiers à tester + dossiers pour les rapports

---
+++ moduleTris/tri_sort_python.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#-*- coding: utf8 -*-
+"""
+    ## Procédure tri sort() de python
+"""
+pass
+
+def fctSort(liste):
+	'''
+	mise en forme de la fonction sort pour effectuer le test de mesure de temps
+	'''
+	liste.sort()
+	return liste
