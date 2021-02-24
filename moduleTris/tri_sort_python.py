5f7ef545ac5958dcf07dc88c912062af39f87fc4
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-02-24 12:10:23 +0100

    :memo: MAJ commentaires tri_sort_python.py

--- moduleTris/tri_sort_python.py
+++ moduleTris/tri_sort_python.py
@@ -5,9 +5,23 @@
 """
 pass
 
-def fctSort(liste):
-	'''
-	mise en forme de la fonction sort pour effectuer le test de mesure de temps
-	'''
+def fctSort(liste:list)->list:
+	"""
+Mise en forme de la fonction sort pour effectuer le test de mesure de temps.
+	
+**Paramètres** liste : une liste	
+**Sorties** la liste triée	
+**Exemples**	
+>>> my_tab_to_sort = [5,1,2,4,3]
+[1,2,3,4,5]
+
+    """
+	pass
+
 	liste.sort()
 	return liste
+
+if __name__=="__main__" :
+    tab_a_trier=[5,4,3,2,1]
+    print(tab_a_trier)
+    print(fctSort(tab_a_trier))
