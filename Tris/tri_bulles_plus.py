e23ca2c96ae0c0ac95bd01d6735a081a035f8a48
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-02-25 09:49:09 +0100

    :poop: tri_bulles_plus() avec spy KO

--- Tris/tri_bulles_plus.py
+++ Tris/tri_bulles_plus.py
@@ -42,24 +42,34 @@ def tri_bulles_plus(tab:list)->list:
     #espion
     global spy
     spy = 0
+    my_ext_spy = 0    
     # On ne trie le tableau que s'il a plus qu'un seul élément
     if n>=2 :
         # Traverser tous les éléments du tableau dans le pire cas
         # mais on s'arrete dès que le tableau est trié
         i=0
-        while (i<n and (ui.isSorted(tab)==False)):
-            # INVARIANT :             
-            # Les éléments n-i à n sont triés
-            #print("i=",i," : ",tab)
-            assert ui.isSorted(tab[n-i:n]),"Le tableau n'est pas trié de n-i à n"
-            assert len(tab)==n,"Le nombre d'éléments du tableau a changé"
-            spy += 1 #espion            
-            for j in range(0, n-i-1):
-                spy += 1  #espion          
-                # Si l'élément trouvé est plus grand que le suivant on échange les deux
-                if tab[j] > tab[j+1] :
-                    #tab[j], tab[j+1] = tab[j+1], tab[j]
-                    ui.permuteTab_i_j(tab,j,j+1)
+        #while (i<n and (ui.isSorted(tab)==False)):        
+        for i in range(n):
+            #print('1e spy tribullesplus : ',spy)
+            my_ext_spy = spy
+            if (ui.isSorted(tab)==True):
+                break
+            else :
+                # INVARIANT :             
+                # Les éléments n-i à n sont triés
+                #print("i=",i," : ",tab)
+                assert ui.isSorted(tab[n-i:n]),"Le tableau n'est pas trié de n-i à n"
+                assert len(tab)==n,"Le nombre d'éléments du tableau a changé"
+                #print("tri bulles plus")
+                spy += 1 #espion           
+                for j in range(0, n-i-1):
+                    spy += 1  #espion                          
+                    # Si l'élément trouvé est plus grand que le suivant on échange les deux
+                    if tab[j] > tab[j+1] :
+                        #tab[j], tab[j+1] = tab[j+1], tab[j]
+                        ui.permuteTab_i_j(tab,j,j+1)
+                #print('2e spy tribullesplus : ',spy)
+                #my_ext_spy = spy
             i+=1
 
     #postconditions
@@ -68,6 +78,8 @@ def tri_bulles_plus(tab:list)->list:
     # il faudrait vérifier que les éléments sont restés ceux de départ peut être avec une fonction à part ?        
 
     return [tab,spy]
+    #return [tab,sum_spy]
+    #return [tab,my_ext_spy]
 
 if __name__=="__main__" :
     tab_a_trier=[5,4,3,2,1]
