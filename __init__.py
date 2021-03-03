80310605c3f86ba19e18aa01169629b4d3aedccd
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-03-03 23:03:08 +0100

    :construction: ajout d'un __init__.py pour la documentation

---
+++ __init__.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+#-*- coding: utf8 -*-
+
+# @author : Sébastien LOZANO
+
+# Source pour l'écriture d'un REAMDE en mardown : https://www.makeareadme.com/ 
+# Génère la page d'accueil de la documentation pdoc3
+
+"""
+# DIU EIL 2020/2022 
+## BLOC2 - Projet Compléxité  
+Le projet consiste à construire un sujet de projet visant à mesurer empiriquement les temps de calcul
+d’algorithmes de tri, et de comparer ces temps de calcul avec les résultats théoriques.
+
+## Auteur
+Fabien SERENKO & Sébastien LOZANO
+
+## Installation - Utilisation
+Pour générer la documentation il faut installer le paquet python [pdoc3](https://pdoc3.github.io/pdoc/)
+
+```bash
+pip3 install pdoc
+```
+Cloner [le dépot](https://github.com/slozano54/DIUEIL-TP_tris) ou télécharger l'[archive zip](https://github.com/slozano54/DIUEIL-TP_tris/archive/master.zip).
+
+À la racine du projet, lancer le script python **programmePrincipal**
+
+```bash
+python3 programmePrincipal.py
+```
+
+## Notes
+Les fichiers de la documentation sont générés dans le dossier **./docs/Tris**
+
+Les fichiers contenant les tableaux à tester sont générés dans le dossier **./csv_files2test**
+
+Les résultats des tests sont générés dans le dossier **./rapports_de_tests**
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+"""
+pass
